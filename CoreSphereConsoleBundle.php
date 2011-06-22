01e1a5aa72db93535ccbc939bcce8926cdf4abd3
Laszlo Korte <laszlokorte@gmail.com>
2011-06-22 21:22:05 +0200

    added license docblocks

--- CoreSphereConsoleBundle.php
+++ CoreSphereConsoleBundle.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the CoreSphereConsoleBundle.
+ *
+ * (c) Laszlo Korte <me@laszlokorte.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace CoreSphere\ConsoleBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
