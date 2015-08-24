4695ec4bb147c1f63ae896d66a538d888f8e93a2
Tomas Votruba <tomas.vot@gmail.com>
2015-08-24 23:31:50 +0200

    tests: StringOutput - test newline as well

--- Tests/Output/StringOutputTest.php
+++ Tests/Output/StringOutputTest.php
@@ -23,7 +23,10 @@ class StringOutputTest extends \PHPUnit_Framework_TestCase
         $text = 'foo';
 
         $output->write($text);
-        $this->assertSame($text, $output->getBuffer(), 'Output gets buffered');
+        $this->assertSame($text, $output->getBuffer());
+
+        $output->write($text, true);
+        $this->assertSame($text . $text . PHP_EOL, $output->getBuffer());
     }
 
 }
