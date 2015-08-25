d3e71310ecfb8b661c1eb023766ad6790d8eb881
Tomas Votruba <tomas.vot@gmail.com>
2015-08-25 16:53:41 +0200

    tests: details

--- Tests/Output/StringOutputTest.php
+++ Tests/Output/StringOutputTest.php
@@ -12,8 +12,9 @@
 namespace CoreSphere\ConsoleBundle\Tests\Output;
 
 use CoreSphere\ConsoleBundle\Output\StringOutput;
+use PHPUnit_Framework_TestCase;
 
-class StringOutputTest extends \PHPUnit_Framework_TestCase
+final class StringOutputTest extends PHPUnit_Framework_TestCase
 {
     public function testWriteRead()
     {
