85feb855db04b4faae381783afd31cd8a31ac727
Tomas Votruba <tomas.vot@gmail.com>
2015-08-26 17:50:30 +0200

    tests: Executer added

---
+++ Tests/Executer/CommandExecutorSource/SomeKernel.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the CoreSphereConsoleBundle.
+ *
+ * (c) Laszlo Korte <me@laszlokorte.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CoreSphere\ConsoleBundle\Tests\Executer\CommandExecutorSource;
+
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+
+final class SomeKernel extends Kernel
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles()
+    {
+        return [
+            new FrameworkBundle()
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(function (ContainerBuilder $containerBuilder) {
+            $containerBuilder->setParameter('kernel.secret', 123);
+        });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCacheDir()
+    {
+        return sys_get_temp_dir() . '/_console_tests/temp';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLogDir()
+    {
+        return sys_get_temp_dir() . '/_console_tests/log';
+    }
+}
