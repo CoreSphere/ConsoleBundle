65ce7e062a04c70c1c3b39e330c32121a9dc911c
Mickaël COLLET <mikl.collet@gmail.com>
2017-12-13 14:43:33 +0100

    Update DataCollector.php
    sf 3.4 + & 4+
    
    (1/1) FatalErrorException
    Error: Class CoreSphere\ConsoleBundle\DataCollector\DataCollector contains 1 abstract method and must therefore be declared abstract or implement the remaining methods (Symfony\Component\HttpKernel\DataCollector\DataCollectorInterface::reset)

--- DataCollector/DataCollector.php
+++ DataCollector/DataCollector.php
@@ -31,4 +31,13 @@ final class DataCollector extends DataCollectorBase
     {
         return 'coresphere_console';
     }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function reset()
+    {
+        $this->data = [];
+    }
+    
 }
